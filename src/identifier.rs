9382a82339436e770f19ce1ce12e41c64a169475
Yurihaia <17830663+Yurihaia@users.noreply.github.com>
2019-11-01 12:00:54 -0400

    fix some stuff with serde and identifiers make arenas and indices transparent

--- src/identifier.rs
+++ src/identifier.rs
@@ -3,17 +3,70 @@ use std::fmt::{
 	Formatter
 };
 
-#[cfg(feature = "serde")]
-use serde::{
-	Serialize,
-	Deserialize
-};
-
-#[cfg_attr(feature= "serde" , derive(Serialize, Deserialize))]
 #[derive(PartialEq, Eq, Clone, Hash, Debug)]
 pub struct Identifier {
 	pub namespace: String,
 	pub path: String
+}
+
+#[cfg(feature = "serde")]
+mod serde_impl {
+	use serde::{
+		Serialize,
+		Deserialize,
+		Serializer,
+		Deserializer,
+		de::{self, Visitor}
+	};
+	use super::*;
+
+	impl Serialize for Identifier {
+		fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
+			serializer.serialize_str(&format!("{}:{}", self.namespace, self.path))
+		}
+	}
+
+	impl <'de> Deserialize<'de> for Identifier {
+		fn deserialize<D>(deserializer: D) -> Result<Self, D::Error> where D: Deserializer<'de> {
+			deserializer.deserialize_str(IdentifierVisitor)
+		}
+	}
+
+	struct IdentifierVisitor;
+
+	impl <'de> Visitor<'de> for IdentifierVisitor {
+		type Value = Identifier;
+
+		fn expecting(&self, f: &mut Formatter) -> Result<(), std::fmt::Error> {
+			write!(f, "a string in the format of an identifier")
+		}
+
+		fn visit_str<E: de::Error>(self, value: &str) -> Result<Self::Value, E> {
+			let mut chars = value.chars();
+			let mut namespace = String::new();
+			loop {
+				match chars.next() {
+					Some(':') => break,
+					Some(v) => match v {
+						'a'..='z'|'-'|'_' => namespace.push(v),
+						e => return Err(de::Error::custom(format!("invalid character '{}'", e)))
+					},
+					None => return Err(de::Error::custom(format!("no namespace")))
+				}
+			};
+			let mut path = String::new();
+			loop {
+				match chars.next() {
+					Some(v) => match v {
+						'a'..='z'|'-'|'_'|'/' => path.push(v),
+						e => return Err(de::Error::custom(format!("invalid character '{}'", e)))
+					},
+					None => break
+				}
+			}
+			Ok(Identifier::new(namespace, path))
+		}
+	}
 }
 
 impl Identifier {
